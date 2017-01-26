16cc3af7358ac12d1ac7f8262d2aa05c7b83a4ed
MichaelCha <mcha@my.apsu.edu>
2017-01-26 17:21:09 -0600

    added button interface for main and credits java. updated andriod mani to add credits activity

--- app/src/main/java/com/example/michaelcha/blackjack/MainActivity.java
+++ app/src/main/java/com/example/michaelcha/blackjack/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.michaelcha.blackjack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button mbutt1, mbutt2, mbutt3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mbutt1 = (Button) findViewById(R.id.button1);
+        mbutt1.setOnClickListener(this);
+
+        mbutt2 = (Button) findViewById(R.id.button2);
+        mbutt2.setOnClickListener(this);
+
+        mbutt3 = (Button) findViewById(R.id.button3);
+        mbutt3.setOnClickListener(this);
+    }
+
+    public void onClick(View v)
+    {
+        if(v==mbutt1)
+            startActivity(new Intent(this, MainActivity.class));
+        else if(v==mbutt2)
+            startActivity(new Intent(this, MainActivity.class));
+        else if(v==mbutt3)
+            startActivity(new Intent(this, credits.class));
+
+    }
+
+
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
     }
 }
+
