bdc90ab1adef99c5b6e9e548955dd25dd36c1f79
mcha83 <mcha@my.apsu.edu>
2017-01-28 14:52:31 -0600

    Add files via upload
    fixed game.java and xml. all activities run

--- java/com/example/michaelcha/blackjack/game.java
+++ java/com/example/michaelcha/blackjack/game.java
@@ -1,6 +1,7 @@
 package com.example.michaelcha.blackjack;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,12 +17,46 @@ public class game extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.howto);
+        setContentView(R.layout.game);
 
-        htButt = (Button) findViewById(R.id.htbutton);
-        htButt.setOnClickListener(this);
+        draw = (Button) findViewById(R.id.button);
+        draw.setOnClickListener(this);
+
+        reset = (Button) findViewById(R.id.button4);
+        reset.setOnClickListener(this);
+
+        quit = (Button) findViewById(R.id.button5);
+        quit.setOnClickListener(this);
 
     }
 
+    public void onClick(View v)
+    {
+
+        if(v==draw)
+        {
+
+        }
+        else if(v==reset)
+        {
+            startActivity(new Intent(this, game.class));
+        }
+        else if(v==quit)
+        {
+            startActivity(new Intent(this, MainActivity.class));
+        }
+
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+    }
 
 }
